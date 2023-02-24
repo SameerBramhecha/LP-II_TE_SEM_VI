3a1a0611745a6f0f1a8fe40327676be6ef15689f
Sameer Manoj Bramhecha <100222138+SameerBramhecha@users.noreply.github.com>
2023-02-24 10:40:36 +0530

    Update Assign2.cpp

--- Assignment_2/Assign2.cpp
+++ Assignment_2/Assign2.cpp
@@ -1,3 +1,27 @@
+// A* Search Algorithm
+
+// let openList equal empty list of nodes
+// let closedList equal empty list of nodes
+// put startNode on the openList (leave it's f at zero)
+// while openList is not empty
+//     let currentNode equal the node with the least f value
+//     remove currentNode from the openList
+//     add currentNode to the closedList
+//     if currentNode is the goal
+//         You've found the exit!
+//     let children of the currentNode equal the adjacent nodes
+//     for each child in the children
+//         if child is in the closedList
+//             continue to beginning of for loop
+//         child.g = currentNode.g + weight b/w child and current
+//         child.h = weight from child to end
+//         child.f = child.g + child.h
+//         if child.position is in the openList's nodes positions
+//             if child.g is higher than the openList node's g
+//                 continue to beginning of for loop
+//         add the child to the openList
+
+
 #include <bits/stdc++.h>
 #include <iostream>
 #include <vector>
@@ -37,16 +61,28 @@ class Graph{
         return H[v];
     }
     void a_star(string source,string destination){
+        /*
+        open_list is a list of nodes that are visited,
+        but their neighbors aren't explored,
+        to start with source node is pushed in open_list
+
+        closed_list is a list of nodes that are visited,
+        and also their neighboring nodes are explored.
+        */
         set<string> open_list,closed_list;
         open_list.insert(source);
+
+        // g contains current distances from start_node to all other nodes
         map<string,int> g;
         g[source] = 0;
+        // parent contains an adjacency map of all nodes
         map<string,string> parent;
         parent[source] = source;
         while(open_list.size()>0){
             string n = "";
             int f = 0;
             for(string v:open_list){ 
+                // find a node with the lowest value of f() - evaluation function
                 if(n=="" || (g[v]+h(v)) < (g[n]+h(n))){
                     n = v;
                     f = g[v] + h(v);
@@ -56,6 +92,8 @@ class Graph{
                 cout<<"NO PATH EXISTS" nl;
                 return;
             }
+            // if the current node is the stop_node
+            // then we begin reconstructin the path from it to the start_node
             if(n==destination){
                 vector<string> recons_path;
                 while(parent[n]!=n){
@@ -74,12 +112,18 @@ class Graph{
                 return;
 
             }
+            // for all neighbors of the current node do
             for(Node v: get_neighbors(n)){
+                // if the current node isn't in both open_list and closed_list
+                // add it to open_list and note n as it's parent
                 if(!closed_list.count(v.name) && !open_list.count(v.name)){
                     open_list.insert(v.name);
                     parent[v.name] = n;
                     g[v.name] = g[n] + v.weight;
                 }
+                // otherwise, check if it's quicker to first visit n, then m
+                // # and if it is, update parent data and g data
+                // # and if the node was in the closed_list, move it to open_list
                 else{
                     if(g[v.name] > (g[n] + v.weight)){
                         g[v.name] = g[n] + v.weight;
@@ -91,6 +135,8 @@ class Graph{
                     }
                 }
             }
+            // remove n from the open_list, and add it to closed_list
+            // # because all of his neighbors were inspected
             open_list.erase(n);
             closed_list.insert(n);
         }
@@ -130,8 +176,36 @@ int main(){
 
 /*
 Input 1 -->
+
+n = 7
+e = 9
+
+Graph: -
+A B 4
+A C 3
+B E 12
+B F 5
+C E 10
+C D 7
+D E 2
+F Z 16
+E Z 5
+
+Heuristic: -
+A 14
+B 12
+C 11
+D 6
+E 4
+F 11
+Z 0
+
+
+Input 2 -->
+
 n = 20
 e = 22
+
 Graph: -
 A Z 75
 A T 118
@@ -156,6 +230,7 @@ U H 98
 H E 86
 V I 92
 I N 87
+
 Heuristic: -
 A 366
 B 0
@@ -178,25 +253,4 @@ U 80
 V 199
 Z 374
 
-Input 2 -->
-Graph: -
-n = 7
-e = 9
-A B 4
-A C 3
-B E 12
-B F 5
-C E 10
-C D 7
-D E 2
-F Z 16
-E Z 5
-Heuristic: -
-A 14
-B 12
-C 11
-D 6
-E 4
-F 11
-Z 0
 */
