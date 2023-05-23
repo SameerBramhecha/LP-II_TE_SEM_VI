28b1db71cdaefa649ce624babca8be5e3e6b3daa
Sameer Manoj Bramhecha <100222138+SameerBramhecha@users.noreply.github.com>
2023-05-23 14:31:02 +0530

    Update Assign4_Branch_And_Bound.cpp

--- AI/Assignment_4/Assign4_Branch_And_Bound.cpp
+++ AI/Assignment_4/Assign4_Branch_And_Bound.cpp
@@ -1,20 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
-class NQueens_BNB
+class BNB
 {
     int n;
 
 public:
-    NQueens_BNB(int n)
+    BNB(int n)
     {
         this->n = n;
     }
-    bool isSafe(int, int, vector<bool> rows, vector<bool> ld, vector<bool> rd);
-    bool solve(vector<vector<char>> &board, int col, vector<bool> &rows, vector<bool> &ld, vector<bool> &rd);
+    void solve(vector<vector<char>> &board, int cols, vector<bool> rows, vector<bool> ld, vector<bool> rd);
+    bool isSafe(int row, int col, vector<bool> cols, vector<bool> ld, vector<bool> rd);
     void print(vector<vector<char>> &board);
 };
-
-bool NQueens_BNB::isSafe(int row, int col, vector<bool> rows, vector<bool> ld, vector<bool> rd)
+bool BNB::isSafe(int row, int col, vector<bool> rows, vector<bool> ld, vector<bool> rd)
 {
     if (rows[row] == true || ld[row + col] == true || rd[col - row + n - 1] == true)
     {
@@ -22,35 +21,36 @@ bool NQueens_BNB::isSafe(int row, int col, vector<bool> rows, vector<bool> ld, v
     }
     return true;
 }
-bool NQueens_BNB::solve(vector<vector<char>> &board, int col, vector<bool> &rows, vector<bool> &ld, vector<bool> &rd)
+
+void BNB::solve(vector<vector<char>> &board, int cols, vector<bool> rows, vector<bool> ld, vector<bool> rd)
 {
-    if (col >= n)
+    if (cols >= n)
     {
-        return true;
+        print(board);
+        return;
     }
     for (int i = 0; i < n; i++)
     {
-        if (isSafe(i, col, rows, ld, rd))
+        if (isSafe(i, cols, rows, ld, rd))
         {
             rows[i] = true;
-            ld[i + col] = true;
-            rd[col - i + n - 1] = true;
-            board[i][col] = 'Q';
-            if (solve(board, col + 1, rows, ld, rd))
-            {
-                return true;
-            }
-            // backtracking now
+            ld[i + cols] = true;
+            rd[cols - i + n - 1] = true;
+            board[i][cols] = 'Q';
+            // now go to next column
+            solve(board, cols + 1, rows, ld, rd);
+            // backtrack
             rows[i] = false;
-            ld[i + col] = false;
-            rd[col - i + n - 1] = false;
-            board[i][col] = '-';
+            ld[i + cols] = false;
+            rd[cols - i + n - 1] = false;
+            board[i][cols] = '-';
         }
     }
-    return false;
 }
-void NQueens_BNB::print(vector<vector<char>> &board)
+
+void BNB::print(vector<vector<char>> &board)
 {
+    cout << "Solution : " << endl;
     for (int i = 0; i < n; i++)
     {
         for (int j = 0; j < n; j++)
@@ -59,24 +59,24 @@ void NQueens_BNB::print(vector<vector<char>> &board)
         }
         cout << endl;
     }
+    cout << endl;
 }
+
 int main()
 {
-    cout << "Enter N: " << endl;
+    cout << "Enter N : " << endl;
     int n;
     cin >> n;
-    NQueens_BNB nq(n);
+    if (n == 2 || n == 3)
+    {
+        cout << "No Solution" << endl;
+        return 0;
+    }
+    BNB b(n);
     vector<vector<char>> board(n, vector<char>(n, '-'));
     vector<bool> rows(n, false);
     vector<bool> ld(2 * n - 1, false);
     vector<bool> rd(2 * n - 1, false);
-    bool ans = nq.solve(board, 0, rows, ld, rd);
-    if (ans)
-    {
-        nq.print(board);
-    }
-    else
-    {
-        cout << "Solution Does Not Exist" << endl;
-    }
+    b.solve(board, 0, rows, ld, rd);
+    return 0;
 }
